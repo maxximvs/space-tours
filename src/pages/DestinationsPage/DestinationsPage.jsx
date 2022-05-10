20fa389d30ea5a037048f749f65428755f2ae603
maxximvs <maxximus.winter@gmail.com>
2022-05-10 10:02:59 +0200

    reworks show/hide planet info logic and adds react-icons

--- src/pages/DestinationsPage/DestinationsPage.jsx
+++ src/pages/DestinationsPage/DestinationsPage.jsx
@@ -2,29 +2,28 @@ import React, { useState } from 'react';
 import '../../shared-styles/shared.css';
 import styles from './destinationspage.module.css';
 
+import destinationsData from '../../data/destinationsData';
+
 import Navigation from '../../components/Navigation/Navigation';
+import planetInfo from '../../assets/images/planet-info.png';
+
+import { FaTimes } from 'react-icons/fa';
 // import planet1 from '../../assets/images/planet-1.png';
 // import planet2 from '../../assets/images/planet-2.png';
 // import planet3 from '../../assets/images/planet-3.png';
-import planetInfo from '../../assets/images/planet-info.png';
-
-import destinations from '../../data/destinations';
 
 const DestinationsPage = () => {
+  const [destinations, setDestinations] = useState(destinationsData);
   const [showDestinationInfo, setShowDestinationInfo] = useState(false);
-  const [destinationInfo, setDestinationInfo] = useState({
-    planetName: '',
-    firstDiscovered: '',
-    discoveredBy: '',
-    planetDescription: ''
-  });
 
   const clickHandler = (id) => {
-    setShowDestinationInfo((prevState) => !prevState);
-    console.log(showDestinationInfo);
-
+    setShowDestinationInfo(true);
     // @todo - close planet info if the same planet is clicked on, change planet info if a different planet is clicked on
     // @todo - display the correct planet info based on the id thats passed in
+  };
+
+  const toggleDestinationInfo = () => {
+    setShowDestinationInfo((prevState) => !prevState);
   };
 
   return (
@@ -34,18 +33,23 @@ const DestinationsPage = () => {
           <Navigation />
           <div className={styles.destinations}>
             {destinations.map((destination) => (
-              <img
-                key={destination.id}
-                src={destination.img}
-                onClick={() => clickHandler(destination.id)}
-              />
+              <div key={destination.id}>
+                <img
+                  src={destination.img}
+                  onClick={() => clickHandler(destination.id)}
+                />
+              </div>
             ))}
           </div>
-          <div className={styles.destinationInfo}>
-            <img
-              src={planetInfo}
-              alt=''
-              className={showDestinationInfo ? '' : styles.hidden}
+          <div
+            className={`${styles.destinationInfo} ${
+              showDestinationInfo ? '' : styles.hidden
+            }`}
+          >
+            <img src={planetInfo} alt='' />
+            <FaTimes
+              className={styles.closeBtn}
+              onClick={toggleDestinationInfo}
             />
             <div className={styles.destinationInfoText}></div>
           </div>
