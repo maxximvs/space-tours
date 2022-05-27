c8d0c939e2edd5f97caa570678571aeaac3074a1
maxximvs <maxximus.winter@gmail.com>
2022-05-27 16:24:53 +0200

    updates Contact with placeholder info

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -7,13 +7,13 @@ const Contact = () => {
       <div className={styles.contactList}>
         <div className={styles.office}>
           <h3>Office</h3>
-          <p>some data</p>
-          <p>some data</p>
+          <p>919 3rd Ave New York City</p>
+          <p>NY, 10022</p>
         </div>
         <div className={styles.getInTouch}>
           <h3>Get in touch</h3>
-          <p>some data</p>
-          <p>some data</p>
+          <p>space.tours@email.com</p>
+          <p>(202) 383-8036</p>
         </div>
       </div>
     </div>
