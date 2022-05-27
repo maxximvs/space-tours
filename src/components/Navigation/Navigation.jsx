d89bc50d59d1435b1a55a6014253a294311c2703
maxximvs <maxximus.winter@gmail.com>
2022-05-27 16:17:33 +0200

    adds brand logo and styling

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import styles from './navigation.module.css';
 
+import { FaSpaceShuttle } from 'react-icons/fa';
+
 import { NavLink } from 'react-router-dom';
 
 const Navigation = () => {
   return (
     <nav className={styles.nav}>
-      <div className={styles.brandLogo}>
-        <NavLink to='/'>Brand Logo</NavLink>
+      <div>
+        <NavLink to='/' className={styles.brandLogo}>
+          <h3>Space Tours</h3>
+          <FaSpaceShuttle size={40} className={styles.brandLogoShuttle} />
+        </NavLink>
       </div>
       <ul className={styles.ul}>
         <li className={styles.li}>
