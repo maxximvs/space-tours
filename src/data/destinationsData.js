2260d9e2b91dda1313b4d1cd213b37d53da363c9
maxximvs <maxximus.winter@gmail.com>
2022-05-29 09:11:24 +0200

    adds extra text to the description

--- src/data/destinationsData.js
+++ src/data/destinationsData.js
@@ -13,7 +13,7 @@ const destinationsData = [
     firstDiscovered: '2045',
     discoveredBy: 'SpaceX',
     planetDescription:
-      'A planet full of life and turquoise lagoons. Great destination for lovers of nature. The many magrove tree like plants make it a great impression on people who visit the planet.',
+      'A planet full of life and turquoise lagoons. Great destination for lovers of nature. The many magrove tree like plants make a great impression on people who visit the planet.',
     img: planet1,
     sqrImg: sqrPlanet1
   },
@@ -23,7 +23,7 @@ const destinationsData = [
     firstDiscovered: '2060',
     discoveredBy: 'NASA',
     planetDescription:
-      'Deserty planet with great vistas and unusual rock formation due to different gravitational forces.',
+      'Deserty planet with great vistas and unusual rock formation due to different gravitational forces. One of the many unique attractions the planet offers is a near zero gravity tour around floating rocks.',
     img: planet2,
     sqrImg: sqrPlanet2
   },
@@ -33,7 +33,7 @@ const destinationsData = [
     firstDiscovered: '2052',
     discoveredBy: 'ESA',
     planetDescription:
-      'Arid desert planet with a lava river flowing across the entire surface of the globe.',
+      'Arid desert planet with a lava river flowing across the entire surface of the globe. The immense heat from the core provides numerous hot springs and spas for relaxation.',
     img: planet3,
     sqrImg: sqrPlanet3
   }
