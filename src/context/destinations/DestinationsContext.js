d25157f8a05bc0a8d3d4c23306b5ef0caf417478
maxximvs <maxximus.winter@gmail.com>
2022-06-02 16:33:00 +0200

    cleans up unused files and comments

--- src/context/destinations/DestinationsContext.js
+++ src/context/destinations/DestinationsContext.js
@@ -21,14 +21,12 @@ export const DestinationsProvider = ({ children }) => {
   useEffect(() => {
     state.destinations.forEach((destination) => {
       if (destination.id === state.destinationId) {
-        // setIsDestinationInfoShown(true);
         dispatch({ type: 'SHOW_DESTINATION_INFO', payload: true });
       }
     });
   }, [state.destinationId]);
 
   const toggleDestinationInfo = () => {
-    // setIsDestinationInfoShown((prevState) => !prevState);
     dispatch({
       type: 'SHOW_DESTINATION_INFO',
       payload: !state.isDestinationInfoShown
@@ -37,7 +35,6 @@ export const DestinationsProvider = ({ children }) => {
 
   const clickHandler = (id) => {
     toggleDestinationInfo();
-    // setDestinationId(id);
     dispatch({ type: 'SET_DESTINATION_ID', payload: id });
   };
 
@@ -46,9 +43,7 @@ export const DestinationsProvider = ({ children }) => {
       value={{
         destinations: state.destinations,
         isDestinationInfoShown: state.isDestinationInfoShown,
-        // setIsDestinationInfoShown: setIsDestinationInfoShown,
         destinationId: state.destinationId,
-        // setDestinationId: setDestinationId,
         toggleDestinationInfo: toggleDestinationInfo,
         clickHandler: clickHandler
       }}
